ffb7ed43f7ca0f1d2b049e84c9d2111d5beb20a6
dtynn <dtynn@163.com>
2013-10-10 13:45:35 +0800

    list

--- handlers/apis.py
+++ handlers/apis.py
@@ -15,7 +15,7 @@ class ApiUpTokenHdl(WwwBaseHdl):
         qConf.SECRET_KEY = secretKey
         policy = qRs.PutPolicy(bucket)
         policy.callbackUrl = '%s/api/q_callback' % (selfHost,)
-        policy.callbackBody = 'etag=$(etag)&opsId=$(persistentId)&file_name=$(x:file_name)'
+        policy.callbackBody = 'etag=$(etag)&opsId=$(persistentId)&file_name=$(x:file_name)&file_size=$(fsize)'
         policy.persistentOps = 'avthumb/m3u8/preset/video_240k;avthumb/android'
         policy.persistentNotifyUrl = '%s/api/q_notify' % (selfHost,)
         uploadToken = policy.token()
@@ -28,10 +28,12 @@ class ApiUpCallbackHdl(WwwBaseHdl):
         etag = self.get_argument('etag', '')
         opsId = self.get_argument('opsId', '')
         file_name = self.get_argument('file_name', '')
+        file_size = self.get_argument('file_size', '')
+        file_size = int(file_size) if file_size else 0
         if etag and opsId:
             uid = 0
             mDataMod = self.settings['mods']['mData']
-            res = 0 if mDataMod.VideoAdd(uid, etag, file_name, opsId) else 1
+            res = 0 if mDataMod.VideoAdd(uid, etag, file_name, file_size, opsId) else 1
             extra = dict()
             extra['key'] = etag
             self.ajax_result(1, res, extra=extra)
